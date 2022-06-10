4373e9012de41e71aa304de9b5ff3ba7b4de7ae6
ildar-icoosoft <ildar.icoosoft@gmail.com>
2022-06-11 02:14:50 +0500

    feat: При старте игры шарик прилипает к лопатке и по клику мыши (либо по enter) можно кинуть шарик (#18)

--- js/ball.js
+++ js/ball.js
@@ -1,9 +1,6 @@
 'use strict';
 
 const BALL_WIDTH = 20;
-const BALL_SPEED = 6;
-const BALL_X = 400;
-const BALL_Y = 400;
 
 /**
  * Ball object.
@@ -14,10 +11,10 @@ export class Ball {
   }
 
   reset() {
-    this.x = BALL_X;
-    this.y = BALL_Y;
-    this.xStep = BALL_SPEED;
-    this.yStep = BALL_SPEED;
+    this.x = 0;
+    this.y = 0;
+    this.xStep = 0;
+    this.yStep = 0;
     this.width = BALL_WIDTH;
   }
 
