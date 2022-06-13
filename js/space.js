535a6882785bd3dce2efffc74ca06da70175c767
ildar-icoosoft <ildar.icoosoft@gmail.com>
2022-06-13 19:00:12 +0500

    feat: Масштабирование холста до размеров окна браузера (#23)

--- js/space.js
+++ js/space.js
@@ -5,15 +5,45 @@ export const SPACE_HEIGHT = 600;
 
 export class Space {
   constructor(game, containerId) {
+    /**
+     * Ширина холста без учёта масштабирования
+     * @type {number}
+     */
     this.width = SPACE_WIDTH;
+
+    /**
+     * Высота холста без учёта масштабирования
+     * @type {number}
+     */
     this.height = SPACE_HEIGHT;
 
-    this.canvas = document.createElement('canvas');
-    this.canvas.width = this.width;
-    this.canvas.height = this.height;
+    this.ratio = 1;
 
-    document.getElementById(containerId).appendChild(this.canvas);
+    this.container = document.getElementById(containerId);
+
+    this.canvas = document.createElement('canvas');
+
+    this.container.appendChild(this.canvas);
     this.context = this.canvas.getContext('2d');
+
+    this.updateCanvasSize();
+    window.addEventListener('resize', () => {
+      this.updateCanvasSize();
+      game.layers.draw();
+    })
+  }
+
+  /**
+   * Меняем размер холста под размеры контейнера.
+   * Всё содержимое холста масштабируется.
+   */
+  updateCanvasSize() {
+    this.ratio = Math.min(this.container.clientWidth / this.width, this.container.clientHeight / this.height);
+
+    this.canvas.width = this.width * this.ratio;
+    this.canvas.height = this.height * this.ratio;
+
+    this.context.scale(this.ratio, this.ratio);
   }
 
   clear() {
